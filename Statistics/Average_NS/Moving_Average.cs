102c2ac9e0907fc30e92eaedd400792d4c5db6e3
Julian B <bechti44@googlemail.com>
2022-08-24 14:44:38 +0200

    added moving average

---
+++ Statistics/Average_NS/Moving_Average.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Statistics.Objects;
+
+namespace Statistics.Average_NS
+{
+    public class Moving_Average
+    {
+        public Moving_Average(TimeSpan totalTime, TimeSpan valueResolution)
+        {
+            this.TotalTime = totalTime;
+            this.ValueResolution = valueResolution;
+        }
+        // "Settings"
+        /// <summary>
+        ///  
+        /// </summary>
+        TimeSpan TotalTime { get; set; }
+        TimeSpan ValueResolution { get; set; }
+        // Working variables
+        Queue<TimeSpot_Value<double>> queue = new Queue<TimeSpot_Value<double>>();
+        private TimeSpot_Value<double> spotValue;
+        private int ResolutionValuePoints = 0;
+        public double AddValuePoint(double value)
+        {
+            return AddValuePoint(value, DateTime.Now);
+        }
+        public double AddValuePoint(double value, DateTime timeStamp)
+        {
+            // create new spot valu if its null
+            //if (this.spotValue.Equals(default(SpotValue<double>))) spotValue = new SpotValue<double>(timeStamp,0);
+            throw new NotImplementedException();
+            return 0;
+        }
+    }
+}
---
+++ Statistics/Objects/TimeSpot_Value.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Statistics.Objects
+{
+    internal struct TimeSpot_Value<T>
+    {
+        internal TimeSpot_Value(DateTime time, T value)
+        {
+            this.Time = time;
+            this.Value = value;
+        }
+        DateTime Time { get; set; }
+        T Value { get; set; }
+    }
+}
