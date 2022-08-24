2ce542a4d587999f76c82b456250c17b182111f4
Julian B <bechti44@googlemail.com>
2022-08-24 14:45:09 +0200

    added progressing average (simmilar to moving but without fixed time window)

---
+++ Statistics/Average_NS/Progressing_Average.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Statistics.Average_NS
+{
+    internal class Progressing_Average_Double
+    {
+        public Progressing_Average_Double()
+        {
+            _Count = 0;
+            Value = 0;
+        }
+        public double Value { get; private set; }
+        private int _Count { get; set; }
+        public void AddValue(double input)
+        {
+            if (_Count == int.MaxValue)
+            {
+                throw new IndexOutOfRangeException("max amount has been reached! use preciseaverage or moving avg instead!");
+            }
+            _Count++;
+            Value += (input - Value) / _Count;
+        }
+    }
+    class Progressing_Average_Decimal
+    {
+        public Progressing_Average_Decimal()
+        {
+            _Count = 0;
+            Value = 0;
+        }
+        public decimal Value { get; private set; }
+        private decimal _Count { get; set; }
+        public void AddValue(decimal input)
+        {
+            Value += (input - Value) / _Count;
+        }
+    }
+}
