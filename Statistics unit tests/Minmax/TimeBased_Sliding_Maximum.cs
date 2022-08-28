f76e9ae3ca2b4aafbc8e45e49a3371bc1d2c3f93
Julian B <bechti44@googlemail.com>
2022-08-28 14:11:13 +0200

    added time based sliding maximum

---
+++ Statistics unit tests/Minmax/TimeBased_Sliding_Maximum.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Statistics_unit_tests.Minmax
+{
+    public class TimeBased_Sliding_Maximum
+    {
+        
+}
---
+++ Statistics/MinMax_NS/TimeBased_Sliding_Maximum.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Statistics.MinMax_NS
+{
+    internal class TimeBased_Sliding_Maximum
+    {
+        public TimeBased_Sliding_Maximum(TimeSpan duration)
+        {
+            CurrentMaximum = 0;
+            Duration = duration;
+        }
+        public double CurrentMaximum { get; set; }
+        public TimeSpan Duration { get; set; }
+
+        private List<Objects.TimeSpot_Value<double>> maximumValues = new List<Objects.TimeSpot_Value<double>>();
+        /// <summary>
+        /// this function assumes the dateTime of the added point is right now
+        /// good for realtime / sensor data
+        /// </summary>
+        /// <param name="input"></param>
+        public void AddValue(double input)
+        {
+            Objects.TimeSpot_Value<double> value = new Objects.TimeSpot_Value<double> {Time = DateTime.Now, Value = input };
+            AddValue(value);
+        }
+        /// <summary>
+        /// this function evaluates the time of the added input (good for historic data)
+        /// </summary>
+        /// <param name="input"></param>
+        public void AddValue(Objects.TimeSpot_Value<double> input)
+        {
+            // calculate the oldest date to keep in tracking list
+            DateTime targetDate = input.Time - Duration;
+            // maximum logic
+            if (input.Value >= CurrentMaximum)
+            {
+                // new maximum found, clear tracking list
+                // this is the best case scenario
+                maximumValues.Clear();
+                CurrentMaximum = input.Value;
+            }
+            else
+            {
+                // unfortunately, no new maximum was found
+                // go backwards the maximum tracking list and check for smaller values
+                // clear the list of all smaller values. The list should therefore always
+                // be in descending order
+                for (int b = maximumValues.Count - 1; b >= 0; b--)
+                {
+                    if (maximumValues[b].Value <= input.Value)
+                    {
+                        // a lower value has been found. We have a newer, higher value
+                        // clear this waste value from the tracking list
+                        maximumValues.RemoveAt(b);
+                    }
+                    else
+                    {
+                        // there are no more lower values. 
+                        // stop looking for smaller values to save time
+                        break;
+                    }
+                }
+            }
+            // append new value to tracking list, no matter if higher or lower
+            // all future values might be lower
+            maximumValues.Add(input);
+            // check if the oldest value is too old to be kept in the tracking list
+            while (maximumValues[0].Time < targetDate)
+            {
+                // oldest value is to be removed
+                maximumValues.RemoveAt(0);
+                // update maximum
+                CurrentMaximum = maximumValues[0].Value;
+            }
+        }
+    }
+}
