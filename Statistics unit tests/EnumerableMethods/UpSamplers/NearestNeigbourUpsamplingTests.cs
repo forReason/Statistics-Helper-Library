447650261dc2092e1b18557004d7770f616bae1c
Julian B <bechti44@googlemail.com>
2024-04-16 11:20:43 +0200

    using cleanup

--- Statistics unit tests/EnumerableMethods/UpSamplers/NearestNeigbourUpsamplingTests.cs
+++ Statistics unit tests/EnumerableMethods/UpSamplers/NearestNeigbourUpsamplingTests.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using QuickStatistics.Net.EnumerableMethods.UpSamplers;
 using Xunit;
 
--- Statistics unit tests/EnumerableMethods/UpSamplers/SplineInterpolationUpsamplingTests.cs
+++ Statistics unit tests/EnumerableMethods/UpSamplers/SplineInterpolationUpsamplingTests.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Text;
 using QuickStatistics.Net.EnumerableMethods.UpSamplers;
 using Xunit;
 
